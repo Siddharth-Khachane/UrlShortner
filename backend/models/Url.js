e0a540b7437f2a9ff8d34bbf881efe924c41f274
Siddharth-Khachane <69320664+Siddharth-Khachane@users.noreply.github.com>
2025-02-05 11:34:26 -0800

    Delete backend directory

--- backend/models/Url.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-const UrlSchema = new mongoose.Schema({
-    originalUrl: { type: String, required: true },
-    shortId: { type: String, required: true, unique: true },
-});
-
-module.exports = mongoose.model("Url", UrlSchema);
